f0742a8cc1ef1b339c9dbf6caca9e1d326ce802b
Ben Chou <ben7152000@gmail.com>
2021-07-31 13:00:25 +0800

    client vue init

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+//deploy.sh
+
+#!/usr/bin/env sh
+
+# 當發生錯誤時終止腳本運行
+set -e
+
+# 打包
+npm run build
+
+# 移動至到打包後的dist目錄
+cd dist
+git init
+git add -A
+git commit -m 'deploy'
+
+# 部署到 https://github.com/ben7152000/blog-vue.git 分支為 gh-pages
+
+git push -f https://github.com/ben7152000/blog-vue.git
+
+cd -
